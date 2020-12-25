c63a02195126b0b0a6138daac9a9dbecb1327d3b
Alex-Muirhead <alexandermuirhead@hotmail.com>
2020-12-26 09:41:37 +1000

    Create executable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::env;
+
+use cryptopals::hex_to_base64;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let input = &args[1];  // The program name is the 0th value
+    println!("The encoded string is {}", hex_to_base64(&input));
 }

d6e6f7c0c317f52170dc9ba54414a7a7f94a3411
Alex-Muirhead <alexandermuirhead@hotmail.com>
2021-02-06 11:43:50 +1000

    Change to active inputs

--- src/bin/1-2.rs
+++ src/bin/1-2.rs
@@ -1,13 +1,24 @@
-use std::env;
+use std::io;
 
 use cryptopals::EncodedData;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let mut arg_one = String::new();
+    let mut arg_two = String::new();
+
+    println!("Input first data string:");
+    io::stdin()
+        .read_line(&mut arg_one)
+        .expect("Failed to read line");
+
+    println!("Input second data string:");
+    io::stdin()
+        .read_line(&mut arg_two)
+        .expect("Failed to read line");
 
     // The program name is the 0th value
-    let input_one = EncodedData::from_hex(&args[1]);
-    let input_two = EncodedData::from_hex(&args[2]);
+    let input_one = EncodedData::from_hex(&arg_one.trim_end());
+    let input_two = EncodedData::from_hex(&arg_two.trim_end());
 
     let crossed_data = input_one ^ input_two;
 
